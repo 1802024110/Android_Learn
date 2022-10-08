837bb839ba73f252f454c85184fa083c6a5b4c36
NoDream <1802024110@qq.com>
2022-10-08 15:29:02 +0800

    有bug

--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.kt
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.kt
@@ -6,29 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-// FruitAdapter的参数是传入数据源。
 class FruitAdapter(val fruitList:List<Fruit>):RecyclerView.Adapter<FruitAdapter.ViewHolder>(){
-    // View参数通常是RecyclerView子项的最外层布局，这样就能在内部使用findViewById()找到ImageView和TextView.
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val fruitImage : ImageView = view.findViewById(R.id.fruitImage)
         val fruitName : TextView = view.findViewById(R.id.fruitName)
     }
 
-    // 该方法用于将布局传入构造函数最后将加载好控件的ViewHolder实例返回
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
         .inflate(R.layout.fruit_item,parent,false)
+
+        val viewHolder = ViewHolder(view)
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            val fruit = fruitList[position]
+            Toast.makeText(parent.context,"你点击了文本${fruit.name}",Toast.LENGTH_SHORT).show()
+        }
+        viewHolder.fruitImage.setOnClickListener  {
+            val position = viewHolder.adapterPosition
+            val fruit = fruitList[position]
+            Toast.makeText(parent.context,"你点击了图片${fruit.name}",Toast.LENGTH_SHORT).show()
+        }
+
         return ViewHolder(view)
     }
 
-    // 每个子项被滚到屏幕内时执行，通过position获得当前子项index.然后设置image和name
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val fruit = fruitList[position]
         holder.fruitImage.setImageResource(fruit.imageId)
         holder.fruitName.text = fruit.name
     }
 
-    // 该方法用于获取数据源长度
     override fun getItemCount(): Int = fruitList.size
 }
--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -4,31 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
     private val fruitList = ArrayList<Fruit>().apply {
         repeat(2) {
-            add(Fruit("Apple", R.drawable.apple_pic))
-            add(Fruit("Banana", R.drawable.banana_pic))
-            add(Fruit("Orange", R.drawable.orange_pic))
-            add(Fruit("Watermelon", R.drawable.watermelon_pic))
-            add(Fruit("Pear", R.drawable.pear_pic))
-            add(Fruit("Grape", R.drawable.grape_pic))
-            add(Fruit("Pineapple", R.drawable.pineapple_pic))
-            add(Fruit("Strawberry", R.drawable.strawberry_pic))
-            add(Fruit("Cherry", R.drawable.cherry_pic))
-            add(Fruit("Mango", R.drawable.mango_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Apple")), R.drawable.apple_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Banana")), R.drawable.banana_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Orange")), R.drawable.orange_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Watermelon")), R.drawable.watermelon_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Pear")), R.drawable.pear_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Grape")), R.drawable.grape_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Pineapple")), R.drawable.pineapple_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Strawberry")), R.drawable.strawberry_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Cherry")), R.drawable.cherry_pic))
+            add(Fruit(getRandomLengthString(getRandomLengthString("Mango")), R.drawable.mango_pic))
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val layoutManager = LinearLayoutManager(this)
-        // 设置排列方向是垂直
-        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+//      更改为网格布局
+        val layoutManager = StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = layoutManager
         val adapter = FruitAdapter(fruitList)
         recyclerView.adapter = adapter
+    }
+
+//  将水果名字随机重复。
+    private fun getRandomLengthString(str:String):String{
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n){
+            builder.append(str)
+        }
+        return builder.toString()
     }
 }
