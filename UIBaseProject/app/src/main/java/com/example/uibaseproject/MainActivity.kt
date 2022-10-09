a8abb331a6568a09baf73e834582f198b3ac618a
NoDream <1802024110@qq.com>
2022-10-09 09:48:47 +0800

    PDF

--- UIBaseProject/app/src/main/java/com/example/uibaseproject/MainActivity.kt
+++ UIBaseProject/app/src/main/java/com/example/uibaseproject/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.uibaseproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    //
+    private val msgList = ArrayList<Msg>().apply {
+        add( Msg("Hello",Msg.TYPE_RECEIVED))
+        add( Msg("HI",Msg.TYPE_SENT))
+        add( Msg("小明",Msg.TYPE_RECEIVED))
+        add( Msg("小红",Msg.TYPE_SENT))
+        add( Msg("在做什么",Msg.TYPE_RECEIVED))
+        add( Msg("写代码",Msg.TYPE_SENT))
+    }
+    private var adapter:MsgAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val layoutManager  = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+        adapter = MsgAdapter(msgList)
+        recyclerView.adapter = adapter
+
+        val send = findViewById<Button>(R.id.send)
+        send.setOnClickListener{
+            val inputText = findViewById<EditText>(R.id.inputText)
+            val content = inputText.text.toString()
+            if(content.isNotEmpty()){
+                val msg = Msg(content,Msg.TYPE_SENT)
+                msgList.add(msg)
+                adapter?.notifyItemInserted(msgList.size-1)
+                recyclerView.scrollToPosition(msgList.size -1)
+                inputText.setText("")
+            }
+        }
     }
 }
--- UIBaseProject/app/src/main/java/com/example/uibaseproject/MsgAdapter.kt
+++ UIBaseProject/app/src/main/java/com/example/uibaseproject/MsgAdapter.kt
@@ -5,16 +5,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class MsgAdapter(val msgList:List<Msg>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-//    inner class LeftViewHolder(view: View,val leftMsg : TextView=view.findViewById(R.id.leftMsg)) : RecyclerView.ViewHolder(view)
-    inner class LeftViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val leftMsg: TextView = view.findViewById(R.id.leftMsg)
-    }
+//    左边控件内部类，用于缓存控件
+    inner class LeftViewHolder(view: View,val leftMsg : TextView=view.findViewById(R.id.leftMsg)) : RecyclerView.ViewHolder(view)
+//    右边控件内部类，用于缓存控件
     inner class RightViewHolder(view: View,val rightMsg : TextView=view.findViewById(R.id.rightMsg)) : RecyclerView.ViewHolder(view)
-
+//    获得是接受还是发送
     override fun getItemViewType(position: Int): Int = msgList[position].type
-
+// 根据不同类型来加载不同布局
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder  = when(viewType){
         Msg.TYPE_RECEIVED -> LeftViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.msg_left_item,parent,false))
         else -> RightViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.msg_right_item,parent,false))
@@ -23,11 +23,11 @@ class MsgAdapter(val msgList:List<Msg>) : RecyclerView.Adapter<RecyclerView.View
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val msg = msgList[position]
         when(holder){
-            LeftViewHolder -> holder
+            is LeftViewHolder -> holder.leftMsg.text = msg.context
+            is RightViewHolder -> holder.rightMsg.text = msg.context
+            else -> throw IllegalArgumentException()
         }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = msgList.size
 }
