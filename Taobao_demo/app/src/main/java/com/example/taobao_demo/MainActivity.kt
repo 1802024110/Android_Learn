cc7ff790d917c72abce259cd1df965a15d7f09cb
NoDream <1802024110@qq.com>
2022-10-12 22:06:09 +0800

    g

--- Taobao_demo/app/src/main/java/com/example/taobao_demo/MainActivity.kt
+++ Taobao_demo/app/src/main/java/com/example/taobao_demo/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.taobao_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.taobao_demo.databinding.ActivityMainBinding
+import com.example.taobao_demo.databinding.NavBarBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val barBinding = mainBinding.bar
+        setContentView(mainBinding.root)
+        barBinding.homeButton.setOnClickListener(this)
+        barBinding.weitaoButton.setOnClickListener(this)
+        barBinding.xiaoxiButton.setOnClickListener(this)
+        barBinding.gwucheButton.setOnClickListener(this)
+        barBinding.wodeButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Log.i(TAG,"你点了$v")
     }
 }
