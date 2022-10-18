aee79d960dfa4a058ae92cdfac27e6e3c1867471
NoDream <1802024110@qq.com>
2022-10-18 11:53:19 +0800

    1

--- MysViewPage2/app/src/main/java/com/example/mysviewpage2/MainActivity.kt
+++ MysViewPage2/app/src/main/java/com/example/mysviewpage2/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.mysviewpage2
-
-import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log.d
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
-    // 使用主线程的循环器
-    private val myHandler = Handler(Looper.getMainLooper())
-
+//    在主线程创建循环器
+    private var mHandler = Handler(Looper.getMainLooper())
+    private lateinit var viewPage2:ViewPager2
     private val datas = ArrayList<Int>().apply {
         add(R.drawable.tx1)
         add(R.drawable.tx2)
@@ -21,14 +23,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewPage2 = findViewById<ViewPager2>(R.id.viewpager2)
+        viewPage2 = findViewById<ViewPager2>(R.id.viewpager2)
         val myViewPage2Adapter = MyViewPage2Adapter(datas)
         viewPage2.adapter = myViewPage2Adapter
     }
 
-    private val runnable:Runnable = object : Runnable{
+//    软件启动时回调
+    override fun onResume(){
+        super.onResume()
+    mHandler.postDelayed(runnable,500)
+}
+//    退出时移除循环器
+    override fun onPause() {
+        super.onPause()
+        mHandler.removeCallbacks(runnable)
+    }
+
+    private val runnable: java.lang.Runnable = object : java.lang.Runnable {
         override fun run() {
-            TODO("Not yet implemented")
+            //获得轮播图当前的位置
+            var currentPosition = viewPage2.currentItem
+            currentPosition++
+            viewPage2.setCurrentItem(currentPosition, true)
+            mHandler.postDelayed(this, 5000)
         }
     }
 }
---
+++ viewPageTest/app/src/main/java/com/example/viewpagetest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.viewpagetest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val viewPager:ViewPager2 = findViewById(R)
+    }
+}
