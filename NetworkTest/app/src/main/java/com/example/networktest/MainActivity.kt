e029af0c9908a02c7e1d41b2d85fc1d1fd0b61ca
NoDream <1802024110@qq.com>
2022-11-22 11:02:30 +0800

    简单使用okhttp

--- NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -22,46 +20,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendRequestWithHttpUrl(url : String){
         thread {
-            // 预定义一个HttpURLConnection对象，方便下边调用
-            var connection : HttpURLConnection? = null
             try {
-                // 字符缓冲区，用于保存返回内容
-                val response = StringBuffer()
-                // 构建一个Url对象
-                val url = URL(url)
-                // 实例HttpURLConnection对象
-                connection = url.openConnection() as HttpURLConnection
-                // 设置连接超时时间
-                connection.connectTimeout = 8000
-                // 设置读取超时时间
-                connection.readTimeout = 8000
-                // 打开输入流
-                val input = connection.inputStream
-                // 创建使用默认大小的输入缓冲区的缓冲字符输入流。
-                val reader = BufferedReader(InputStreamReader(input))
-
-                reader.use {
-                    // 遍历
-                    reader.forEachLine {
-                        // 添加到缓冲区
-                        response.append(it)
-                    }
+                // 创建OkHttpClient
+                val client = OkHttpClient()
+                // 创建构造器
+                val request = Request.Builder().apply {
+                    url(url)
+                }.build()
+                // 发送请求
+                val response = client.newCall(request).execute()
+                // 获得返回值
+                val responseData = response.body?.string()
+                // 判断是否为空
+                responseData?.let {
+                    // 渲染
+                    showResponse(it)
                 }
-                // 调用showResponse
-                showResponse(response.toString())
             }catch (e:Exception){
                 e.printStackTrace()
-            }finally {
-                // 关闭链接
-                connection?.disconnect()
             }
         }
     }
     private fun showResponse(response: String) {
-        // 在UI线程上运行指定的操作。如果当前线程是UI线程，那么动作将立即执行。
         runOnUiThread {
             val responseText:TextView = findViewById(R.id.responseText)
-            // 在这里进行UI操作，将结果显示到界面上
             responseText.text = response
         }
     }
