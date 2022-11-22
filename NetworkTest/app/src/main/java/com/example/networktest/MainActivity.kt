5f69b33e20917fbe43178f72ba0ffa87792867ea
NoDream <1802024110@qq.com>
2022-11-22 16:38:39 +0800

    初使用pull解析xml数据

--- NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.networktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.StringReader
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -14,37 +18,67 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val sendRequestBtn: Button = findViewById(R.id.sendRequestBtn)
         sendRequestBtn.setOnClickListener {
-            sendRequestWithHttpUrl("https://007666.xyz/")
+            sendRequestWithHttpUrl("https://jsdelivr.nodream.cf/gh/1802024110/GitHub_Oss@main/file/安卓第一行代码解析xml数据.xml")
         }
     }
 
     private fun sendRequestWithHttpUrl(url : String){
         thread {
             try {
-                // 创建OkHttpClient
                 val client = OkHttpClient()
-                // 创建构造器
                 val request = Request.Builder().apply {
                     url(url)
                 }.build()
-                // 发送请求
                 val response = client.newCall(request).execute()
-                // 获得返回值
                 val responseData = response.body?.string()
-                // 判断是否为空
                 responseData?.let {
-                    // 渲染
-                    showResponse(it)
+                    // 将输出方法改为转换xml
+                    parseXMLWithPull(it)
                 }
             }catch (e:Exception){
                 e.printStackTrace()
             }
         }
     }
-    private fun showResponse(response: String) {
-        runOnUiThread {
-            val responseText:TextView = findViewById(R.id.responseText)
-            responseText.text = response
-        }
+
+    private fun parseXMLWithPull(xmlData: String) {
+        try {
+            // 创建可用于创建XML拉式解析器的PullParserFactory的新实例。
+            val factory = XmlPullParserFactory.newInstance()
+            // 使用当前配置的工厂特性创建XML Pull Parser的新实例。
+            val xmlPullParser = factory.newPullParser()
+            // 将解析器的输入源设置为给定的字符串读取器并重置解析器。
+            xmlPullParser.setInput(StringReader(xmlData))
+            // 返回当前事件的类型(START_TAG, END_TAG, TEXT等)。
+            var eventType = xmlPullParser.eventType
+            var id = ""
+            var name = ""
+            var version = ""
+            // xml文档的逻辑结束
+            while (eventType!=XmlPullParser.END_DOCUMENT){
+                // 对于START_TAG或END_TAG事件，在启用名称空间时返回当前元素的(本地)名称。
+                val nodeName = xmlPullParser.name
+                when (eventType) {
+                    // 读取开始标记时。
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    // 完成解析某个节点
+                    XmlPullParser.END_TAG -> {
+                        if ("app" == nodeName) {
+                            Log.d("MainActivity", "id is $id")
+                            Log.d("MainActivity", "name is $name")
+                            Log.d("MainActivity", "version is $version")
+                        }
+                    }
+                }
+                // 获取下一个解析事件
+                eventType = xmlPullParser.next()
+            }
+        } catch (e:Exception){}
     }
 }
