9e9ddeb2573836206307aa311adc3936282993dd
NoDream <1802024110@qq.com>
2022-11-25 22:38:24 +0800

    viewMode基础

--- ViewModelTest/app/src/main/java/com/example/viewmodeltest/MainActivity.kt
+++ ViewModelTest/app/src/main/java/com/example/viewmodeltest/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.viewmodeltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel:MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val plusOneBth:Button = findViewById(R.id.plusOneBtn)
+        plusOneBth.setOnClickListener {
+            viewModel.counter++
+            refreshCounter()
+        }
+        refreshCounter()
+    }
+    private fun refreshCounter() {
+        val infoText:TextView = findViewById(R.id.infoText)
+        infoText.text = viewModel.counter.toString()
     }
 }
