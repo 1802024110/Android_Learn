15190a386a76e8a55390d271627d9d173569da92
NoDream <1802024110@qq.com>
2022-11-26 16:46:36 +0800

    是啥来着

--- ViewModelTest/app/src/main/java/com/example/viewmodeltest/MainActivity.kt
+++ ViewModelTest/app/src/main/java/com/example/viewmodeltest/MainActivity.kt
@@ -7,47 +7,38 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.content.edit
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    // 预定义ViewModel
     lateinit var viewModel:MainViewModel
-    // 访问和修改Context.getSharedPreferences返回的首选项数据的接口。
     lateinit var sp:SharedPreferences
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // 实例化一个SharedPreferences对象，以访问此活动私有的文件。
         sp = getPreferences(Context.MODE_PRIVATE)
-        // 获取上一次存入的计数器
         val countReserved = sp.getInt("count_reserved", 0)
-        // 将获取到的计算器存入viewModel
         viewModel = ViewModelProvider(this,MainViewModelFactory(countReserved)).get(MainViewModel::class.java)
-
         val plusOneBth:Button = findViewById(R.id.plusOneBtn)
         plusOneBth.setOnClickListener {
-            viewModel.counter++
-            refreshCounter()
+            // 替换
+            viewModel.plusOne()
         }
-
         val clearBtn:Button = findViewById(R.id.clearBtn)
         clearBtn.setOnClickListener {
-            viewModel.counter = 0
-            refreshCounter()
+            // 替换
+            viewModel.clear()
         }
-        refreshCounter()
+        // 替换
+        viewModel.counter.observe(this,Observer{
+            val infoText:TextView = findViewById(R.id.infoText)
+            infoText.text = it.toString()
+        })
     }
-    private fun refreshCounter() {
-        val infoText:TextView = findViewById(R.id.infoText)
-        infoText.text = viewModel.counter.toString()
-    }
-
     override fun onPause() {
         super.onPause()
         sp.edit{
-            putInt("count_reserved", viewModel.counter)
+            putInt("count_reserved",viewModel.counter.value ?: 0)
         }
     }
 }
