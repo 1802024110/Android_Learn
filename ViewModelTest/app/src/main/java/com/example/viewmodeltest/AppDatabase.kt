aad71b74042a37fe1e9661042fbaf05d7a35758b
NoDream <1802024110@qq.com>
2022-11-27 22:39:15 +0800

    1152

--- ViewModelTest/app/src/main/java/com/example/viewmodeltest/AppDatabase.kt
+++ ViewModelTest/app/src/main/java/com/example/viewmodeltest/AppDatabase.kt
@@ -4,17 +4,35 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-@Database(version = 1, entities = [User::class])
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+@Database(version = 3, entities = [User::class,Book::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao():UserDao
+    abstract fun bookDao():BookDao
 
     companion object {
+        val MIGRATION_1_2 = object : Migration(1,2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("create table Book (id integer primary\n" +
+                        " key autoincrement not null, name text not null,\n" +
+                        " pages integer not null)")
+            }
+        }
+        val MIGRATION_2_3 = object : Migration(2,3){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("alter table Book add column author text not null default 'unknown'")
+            }
+        }
+
         private var instance: AppDatabase? = null
-        @Synchronized
+
         fun getDatabase(context: Context): AppDatabase {
             instance?.let {return it}
             return Room.databaseBuilder(context.applicationContext,
                 AppDatabase::class.java, "app_database")
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                 .build().apply {
                     instance = this
                 }
