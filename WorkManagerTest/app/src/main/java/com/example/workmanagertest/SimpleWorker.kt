9c8747035b1f6481e32639fd7412d1d32f5a4779
NoDream <1802024110@qq.com>
2022-11-28 16:48:49 +0800

    kbj

---
+++ WorkManagerTest/app/src/main/java/com/example/workmanagertest/SimpleWorker.kt
@@ -0,0 +1,9 @@
+package com.example.workmanagertest
+
+import androidx.work.Worker
+
+class SimpleWorker : Worker(){
+    override fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+}
